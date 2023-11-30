6cc7a2735ac22fa25013d8012af4f4288c078464
pyroceper <pyroceper@gmail.com>
2023-11-30 11:14:50 -0500

    Added Bert

--- py/main.py
+++ py/main.py
@@ -6,6 +6,9 @@ from svm.svm import SVM
 from cache_model import check_cache
 from pdf_creator import PDF
 from database import Database
+#debug
+#import torch
+#from bert import *
 
 app = Flask(__name__)
 nb = 0
@@ -14,6 +17,8 @@ dt = 0
 svm = 0
 pdf = 0
 db = 0
+#debug
+#bert = 0
 
 @app.route('/train', methods=['POST'])
 def train_model():
@@ -45,6 +50,7 @@ def predict_email():
             nb.predict(email)
             dt.predict(email)
             svm.predict(email)
+            #predict_sentiment(email, bert, tokenizer, device) #bert
         return jsonify({'email': email, 'nb_phish_prob': nb.accuracy, 'nb_result': nb.result, 'dt_phish_prob': dt.accuracy, 'dt_result': dt.result, 'svm_phish_prob': nb.accuracy, 'svm_result': svm.result})
     except Exception as e:
         error_msg = str(e)
@@ -104,5 +110,12 @@ if __name__ == '__main__':
     print("[+] Decision Tree classifier created")
     svm.train_model()
     print("[+] SVM classifier created")
+    #
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #bert = BERTClassifier('bert-base-uncased', 2).to(device)
+    #bert.load_state_dict(torch.load('./cache/bert_classifier.pth'))
+    #bert.eval()
+    #print("[+] BERT Classifier loaded")
+    #
     print("[+] Done!")
     app.run(debug=False)
